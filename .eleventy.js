d68b97ce12d875b07193360d4160a174c009fffd
anelisgh <anelis.gheorghias94@gmail.com>
2026-04-20 21:48:04 +0300

    test

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
+const { HtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
   // Passthrough Copy
   eleventyConfig.addPassthroughCopy("src/assets");
 
+  // Plugins
+  eleventyConfig.addPlugin(HtmlBasePlugin);
+
   return {
+    pathPrefix: "/clinica-kineto/",
     dir: {
       input: "src",
       output: "_site",
